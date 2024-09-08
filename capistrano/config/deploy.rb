a3974b880a8d19eab3668e44d37bd762b51f6916
Gaku Yasui <gaku.yasui@gmail.com>
2024-09-08 23:17:21 +0900

    Update

--- capistrano/config/deploy.rb
+++ capistrano/config/deploy.rb
@@ -7,6 +7,7 @@ set :repo_tree, "capistrano"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
